91659624e90a059180fce3bd12bb6b90c424f6a5
InFTord <60395110+InFTord@users.noreply.github.com>
2020-09-30 11:27:34 +0500

    Команд Хандлер добавлен!

--- bot.js
+++ bot.js
@@ -1,22 +1,35 @@
 const Discord = require('discord.js');
+const fs = require('fs');
 const client = new Discord.Client();
+client.commands = new Discord.Collection();
+const commandFiles = fs.readdirSync('/.commands').filter(file => file.endsWith('.js'));
 const { prefix, token } = require('./config.json');
 
 client.once('ready', () => {
 	console.log('Готов!');
 });
 
+for(const file of commandFiles) {
+	const command = require(`./commands/${file}`);
+	client.commands.set(command.name, command);
+
+}
+
 client.on('message', message => {
 	if(!message.content.startsWith(prefix) || message.author.bot) return;
 	const args = message.content.slice(prefix.length).trim().split(/  +/);
 	const command = args.shift().toLowerCase();
 
-	if(command === 'аргументы') {
-		if (!args.length) {
-			return message.channel.send(`Вы не дали аргументы, ${message.author}!`);
-		}
-		message.channel.send(`Название команды: ${command}\nАргументы: ${args} `);
+	if (!client.commands.has(command)) return;
+	try {
+		client.commands.get(command).execute(message, args);
+		console.log(`Была произведена команда ${command}`);
 	}
-});
+	catch (error) {
+		console.error(error);
+		message.reply('Произошла ошибка жопы');
+	}
+},
+);
 
 client.login(token);
